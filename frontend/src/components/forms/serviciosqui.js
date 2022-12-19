09cc480c6b110efd20fccd6e41e7caf3cf288617
Francisco Rodriguez Almeida <Francisco.Rodriguez@sjd.es>
2022-12-19 15:44:31 +0000

    1912H

--- frontend/src/components/forms/serviciosqui.js
+++ frontend/src/components/forms/serviciosqui.js
@@ -8,127 +8,132 @@ import Switch from "@mui/material/Switch";
 import "./css/serviciosqui.css";
 
 function Serviciosqui(props) {
-    const [servicios, setServicios] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [actualiza, setActualiza] = useState(false);
+	const [servicios, setServicios] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [actualiza, setActualiza] = useState(false);
 
-    const Item = styled(Paper)(({ theme }) => ({
-        ...theme.typography.body2,
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-        height: 60,
-        width: "19%",
-        lineHeight: "60px",
-        marginLeft: 12,
-        marginTop: 12,
-    }));
-    const lightTheme = createTheme({ palette: { mode: "light" } });
+	const Item = styled(Paper)(({ theme }) => ({
+		...theme.typography.body2,
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+		height: 60,
+		width: "19%",
+		lineHeight: "60px",
+		marginLeft: 12,
+		marginTop: 12,
+	}));
+	const lightTheme = createTheme({ palette: { mode: "light" } });
 
-    useEffect(() => {
-        const fetchServicios = async () => {
-            await fetch(
-                "http://" +
-                    process.env.REACT_APP_API_SERVER +
-                    ":" +
-                    process.env.REACT_APP_API_PORT +
-                    "/api/servicios/" +
-                    props.facility
-            )
-                .then((response) => response.json())
-                .then((data) => {
-                    setServicios(data);
-                    setIsLoading(false);
-                });
-        };
-        fetchServicios();
-    }, [actualiza]);
+	useEffect(() => {
+		const fetchServicios = async () => {
+			await fetch(
+				"http://" +
+					process.env.REACT_APP_API_SERVER +
+					":" +
+					process.env.REACT_APP_API_PORT +
+					"/api/servicios/" +
+					props.facility
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					setServicios(data);
+					setIsLoading(false);
+				});
+		};
+		setIsLoading(true);
+		fetchServicios();
+	}, [actualiza]);
 
-    const handleSwitchChange = (event, xkey, state) => {
-        var url =
-            "http://" +
-            process.env.REACT_APP_API_SERVER +
-            ":" +
-            process.env.REACT_APP_API_PORT +
-            "/api/servicios";
+	const handleSwitchChange = (event, xkey, state) => {
+		var url =
+			"http://" +
+			process.env.REACT_APP_API_SERVER +
+			":" +
+			process.env.REACT_APP_API_PORT +
+			"/api/servicios";
 
-        const postData = { facility: props.facility, servicio: xkey };
+		const postData = { facility: props.facility, servicio: xkey };
 
-        if (!state) {
-            const fetchData = async () => {
-                const response = await fetch(url, {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(postData),
-                });
-                setActualiza(!actualiza);
-                const data = await response.json();
-                console.log(data);
-            };
+		if (!state) {
+			const fetchData = async () => {
+				const response = await fetch(url, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(postData),
+				});
+				setActualiza(!actualiza);
+				const data = await response.json();
+				console.log(data);
+			};
+			setIsLoading(true);
+			fetchData();
+		} else {
+			const fetchData = async () => {
+				const response = await fetch(url, {
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(postData),
+				});
+				setActualiza(!actualiza);
+				const data = await response.json();
+				console.log(data);
+			};
+			setIsLoading(true);
+			fetchData();
+		}
+	};
 
-            fetchData();
-        }
-    };
+	return (
+		<Grid sx={{ flexGrow: 1 }} container spacing={0}>
+			<div className="tituloLoading">
+				<Typography
+					sx={{ marginRight: 2 }}
+					fontFamily="Open Sans"
+					fontSize={18}
+					color="navy"
+				>
+					Servicios Quirúrgicos del hospital
+				</Typography>
+				{isLoading && (
+					<ReactLoading type="spin" color="navy" width={24} height={24} />
+				)}
+			</div>
+			<Grid item xs={12}>
+				<Grid container justifyContent="left" spacing={1}>
+					{servicios.map((ser) => (
+						<Item
+							key={ser.XKEY}
+							elevation={6}
+							sx={{ backgroundColor: ser.QUI && "#5CDF63" }}
+						>
+							<div className="tituloContainer">
+								<Typography color="navy" fontFamily="Open Sans" fontSize={16}>
+									{ser.XKEY.substring(3)}
+								</Typography>
+								<div className="switchStyle">
+									<Switch
+										key={ser.XKEY}
+										checked={ser.QUI ? true : false}
+										onChange={(e) =>
+											handleSwitchChange(e, ser.XKEY, ser.QUI ? true : false)
+										}
+									/>
+								</div>
+							</div>
 
-    if (isLoading) {
-        return (
-            <div className="containerLoading">
-                <div className="centerLoading">
-                    <ReactLoading type="spinningBubbles" color="navy" />
-                </div>
-            </div>
-        );
-    } else {
-        return (
-            <Grid sx={{ flexGrow: 1 }} container spacing={0}>
-                <Typography fontFamily="Open Sans" fontSize={18} color="navy">
-                    Servicios Quirúrgicos del hospital
-                </Typography>
-                <Grid item xs={12}>
-                    <Grid container justifyContent="left" spacing={1}>
-                        {servicios.map((ser) => (
-                            <Item
-                                key={ser.XKEY}
-                                elevation={6}
-                                sx={{ backgroundColor: ser.QUI && "#5CDF63" }}
-                            >
-                                <div className="tituloContainer">
-                                    <Typography
-                                        color="navy"
-                                        fontFamily="Open Sans"
-                                        fontSize={16}
-                                    >
-                                        {ser.XKEY.substring(3)}
-                                    </Typography>
-                                    <div className="switchStyle">
-                                        <Switch
-                                            key={ser.XKEY}
-                                            checked={ser.QUI ? true : false}
-                                            onChange={(e) =>
-                                                handleSwitchChange(
-                                                    e,
-                                                    ser.XKEY,
-                                                    ser.QUI ? true : false
-                                                )
-                                            }
-                                        />
-                                    </div>
-                                </div>
-
-                                <Typography
-                                    fontFamily="Open Sans"
-                                    fontSize={12}
-                                >
-                                    {ser.DESCRIPTION}
-                                </Typography>
-                            </Item>
-                        ))}
-                    </Grid>
-                </Grid>
-            </Grid>
-        );
-    }
+							<Typography fontFamily="Open Sans" fontSize={12}>
+								{ser.DESCRIPTION}
+							</Typography>
+						</Item>
+					))}
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default Serviciosqui;
